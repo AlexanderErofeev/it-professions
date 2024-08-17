920acf0aaadb9ff20543a9c154f80d278c1cc95a
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-08-17 15:56:27 +0500

    IP-23 вывод изображения второй колонкой в show

--- resources/views/professions/show.blade.php
+++ resources/views/professions/show.blade.php
@@ -5,9 +5,12 @@
     <div class="container mb-4">
         <div class="row">
             <!-- Blog entries-->
-            <div class="col-lg-12">
+            <div class="col-8">
                 <h1>{{ $profession->title }}</h1>
                 <p>{!! $profession->description !!}</p>
+            </div>
+            <div class="col-4">
+                <img src="{{ Storage::url($profession->image_path) }}" alt="{{ $profession->title }}" class="card-img-top img-fluid">
             </div>
         </div>
     </div>
