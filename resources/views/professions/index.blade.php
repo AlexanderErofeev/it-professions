87d5e19c36c5de7fdb632a2624d800c23b9bfed5
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-08-17 15:58:22 +0500

    IP-23 Удаление неиспользуемых в этой ветке кнопок для редактирования контента из index

--- resources/views/professions/index.blade.php
+++ resources/views/professions/index.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
     <div class="container professions">
-        <h1>Профессии
-            @auth
-                <a href="{{ route('professions.create') }}"><i class="fa-solid fa-plus icon"></i></a>
-            @endauth
-        </h1>
+        <h1>Профессии</h1>
         <article>
             <div class="row row-cols-1 row-cols-md-3 g-4">
                 @foreach($professions as $profession)
@@ -17,16 +13,6 @@
                                 <a href="{{ route('professions.show', $profession) }}"><h5 class="card-title text-center">{{ $profession->title }}</h5></a>
                                 <p class="card-text">{!! $profession->short_description !!}</p>
                             </div>
-                            @auth
-                                <div class="admin card-footer">
-                                    <a href="{{ route('professions.edit', $profession) }}"><i class="fa-regular fa-pen-to-square icon"></i></a>
-                                    <form method="POST" action="{{ route('professions.delete', $profession) }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="fa-regular fa-trash-can delete icon" onclick="return confirm('Вы уверены?')"></button>
-                                    </form>
-                                </div>
-                            @endauth
                         </div>
                     </div>
                 @endforeach
--- resources/views/skills/index.blade.php
+++ resources/views/skills/index.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
     <div class="container skills">
-        <h1>Навыки
-            @auth
-                <a href="{{ route('skills.create') }}"><i class="fa-solid fa-plus icon"></i></a>
-            @endauth
-        </h1>
+        <h1>Навыки</h1>
         <article>
             <div class="row row-cols-1 row-cols-md-3 g-4">
                 @foreach($skills as $skill)
@@ -15,18 +11,8 @@
                             <a href="{{ route('skills.show', $skill) }}"><img src="{{ Storage::url($skill->image_path) }}" alt="{{ $skill->title }}" class="card-img-top img-fluid"></a>
                             <div class="card-body">
                                 <a href="{{ route('skills.show', $skill) }}"><h5 class="card-title text-center">{{ $skill->title }}</h5></a>
-                                <p class="card-text">{{ $skill->short_description }}</p>
+                                <p class="card-text">{!! $skill->short_description !!}</p>
                             </div>
-                            @auth
-                                <div class="admin card-footer">
-                                    <a href="{{ route('skills.edit', $skill) }}"><i class="fa-regular fa-pen-to-square icon"></i></a>
-                                    <form method="POST" action="{{ route('skills.delete', $skill) }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="fa-regular fa-trash-can delete icon" onclick="return confirm('Вы уверены?')"></button>
-                                    </form>
-                                </div>
-                            @endauth
                         </div>
                     </div>
                 @endforeach
