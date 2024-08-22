05660c906034075746280f9e399c2c308631db6c
Денис <d.b.shadrin@urfu.ru>
2024-08-22 21:50:04 +0500

    IP-42 Исправлен баг при работе Unable to locate file in Vite manifest

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,10 @@ import '@fortawesome/fontawesome-free/scss/brands.scss';
 import '@fortawesome/fontawesome-free/scss/regular.scss';
 import '@fortawesome/fontawesome-free/scss/solid.scss';
 import '@fortawesome/fontawesome-free/scss/v4-shims.scss';
+import.meta.glob([
+    '../images/**',
+    '../fonts/**',
+]);
 
 import Alpine from 'alpinejs';
 
