b60be24249d424228f0e7df7a2aad89c0e7c118a
Денис <d.b.shadrin@urfu.ru>
2024-08-24 14:40:24 +0500

    IP-29 Добавлены переводы

--- lang/ru/auth.php
+++ lang/ru/auth.php
@@ -1,20 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
-    'failed' => 'Предоставленные данные не верны',
-    'password' => 'Пароль не верен',
-    'throttle' => 'Большое количество попыток входа. Пожалуйста подождите :seconds seconds и повторите вход.',
-
+    'failed'   => 'Неверное имя пользователя или пароль.',
+    'password' => 'Некорректный пароль.',
+    'throttle' => 'Слишком много попыток входа. Пожалуйста, попробуйте ещё раз через :seconds секунд.',
 ];
