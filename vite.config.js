9085b6159eb49920ebe37000c9fc5c99422144d2
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-08-26 22:27:05 +0500

    Исправлен баг при работе Unable to locate file in Vite manifest

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,15 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/js/ckeditor.config.js',
             ],
             refresh: true,
         }),
         ckeditor5( { theme: require.resolve( '@ckeditor/ckeditor5-theme-lark' ) } ),
     ],
+    server: { // [tl! add:start]
+        hmr: {
+            host: 'localhost',
+        },
+    }, // [tl! add:end]
 });
