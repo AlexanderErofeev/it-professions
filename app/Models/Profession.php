49e7177e3c092f6d8ae678acfeced93556e52feb
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-09-09 16:43:19 +0500

    IP-37 Удаление изображений, при удалении профессии и навыка

--- app/Models/Profession.php
+++ app/Models/Profession.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Profession extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::updated(static function (Profession $item) {
+            if ($item->wasChanged('image_path') && $item->getOriginal('image_path')) {
+                Storage::disk('public')->delete($item->getOriginal('image_path'));
+            }
+        });
+    }
 }
--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -5,9 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Http\Controllers\SkillController;
+use Illuminate\Support\Facades\Storage;
 
 class Skill extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::updated(static function (Skill $item) {
+            if ($item->wasChanged('image_path') && $item->getOriginal('image_path')) {
+                Storage::disk('public')->delete($item->getOriginal('image_path'));
+            }
+        });
+    }
 }
